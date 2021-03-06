b6c53a0679b7c887d6c706f39128e796b79bc43d
TruKat <kati.k220@gmail.com>
2021-03-05 20:48:14 -0700

    ADDED: workouts being added successfully

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,27 +1,44 @@
 const router = require("express").Router();
 const Workout = require("../models/workouts.js");
 
+// finds most recent workout
 router.get("/api/workouts", (req, res) => {
-  Workout.find({})
-    .sort({ date: -1 })
+  Workout.find({}),
+    Workout.aggregate([
+      { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+    ])
+      .sort({ date: -1 })
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+});
+
+  // adds new workout to db
+  router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(
+        req.params.id,
+        { $push: {exercises: req.body } },
+        { new: true }
+    )
     .then((dbWorkout) => {
-      console.log(dbWorkout);
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+});
+
+// creates a workout
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then((dbWorkout) => {
       res.json(dbWorkout);
     })
     .catch((err) => {
       res.status(400).json(err);
     });
 });
-
-router.post("/api/workouts", (req, res) => {
-  Workout.create({})
-    .then((res) => {
-      console.log(res);
-      res.json(res);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
 module.exports = router;
