6816edd5c9980c98c35d23e472301ba6345c9afb
TruKat <kati.k220@gmail.com>
2021-03-12 16:49:24 -0700

    deploying

--- server.js
+++ server.js
@@ -12,10 +12,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
-    useNewUrlParser: true, 
-    useFindAndModify: false 
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.use(require("./routes/api-routes"));
 app.use(require("./routes/html-routes"));
